9f39ed9c08f809c85d4d8f71672357fc408025af
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-08-07 22:43:18 +0900

    Initial Commit

---
+++ DDMysticPuzzle/DDMysticPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDMysticPuzzleApp.swift
+//  DDMysticPuzzle
+//
+//  Created by joe on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct DDMysticPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
