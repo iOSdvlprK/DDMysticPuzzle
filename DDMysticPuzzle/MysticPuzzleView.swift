99c69897ab3f88be8b5b3c1cda2015cf2142306a
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-08-12 23:07:20 +0900

    add digit image assets and update puzzle view
    - added digit0–digit4 image assets to the asset catalog for use as tile images
    - updated MysticPuzzleView to display tiles using the new digit images and refactored the view layout for improved appearance

--- DDMysticPuzzle/MysticPuzzleView.swift
+++ DDMysticPuzzle/MysticPuzzleView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct MysticPuzzleView: View {
+    @StateObject var mysticPuzzleViewModel: MysticPuzzleViewModel = .init()
+    
+    let title = "Mystic Puzzle"
+    let tileDimensions: CGFloat = 70
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.blue)
+                .opacity(0.3).ignoresSafeArea()
+            
+            VStack {
+                Text(title)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                ZStack {
+                    let tiles = mysticPuzzleViewModel.mysticPuzzleModel.tiles
+                    
+                    ForEach(tiles) { tile in
+                        TileView(tileNumber: tile.value, tileDimensions: tileDimensions, offset: tile.currentPoint)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+}
+
+struct TileView: View {
+    let tileNumber: Int
+    let tileDimensions: CGFloat
+    let offset: CGPoint
+    
+    let somePadding: CGFloat = 3
+    let digitResizeFactor: CGFloat = 5 / 2
+    
+    var body: some View {
+        let direction = offset * tileDimensions
+        
+        let deltaX = direction.x + tileDimensions / 2
+        let deltaY = direction.y - 3 * tileDimensions / 2
+        
+        Image("digit\(tileNumber)")
+            .resizable()
+            .renderingMode(.template)
+            .foregroundStyle(.red)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: tileDimensions, height: tileDimensions)
+            .padding()
+            .background(
+                Color.gray
+                    .frame(width: tileDimensions - somePadding, height: tileDimensions - somePadding)
+                    .clipShape(.rect(cornerRadius: 10))
+            )
+            .offset(x: deltaX, y: deltaY)
+            .shadow(color: .black, radius: 1, x: 1, y: 1)
     }
 }
 
